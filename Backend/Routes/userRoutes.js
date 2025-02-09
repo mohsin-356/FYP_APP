6f81e19d1dd2beb0b909b463a882db5a95fcf4f8
mohsin-356 <mohsinsaeed356@gmail.com>
2025-02-09 12:42:31 +0500

    Implement user addition functionality and clean up unused routes in backend

--- Backend/Routes/userRoutes.js
+++ Backend/Routes/userRoutes.js
@@ -3,9 +3,30 @@ const userController=require('../Controllers/userController');
 
 const express = require('express');
 const router = express.Router();
+const multer = require("multer");
+const crypto = require("crypto");
+const path = require("path");
 
-router.route("/addUser").post(userController.addUser);
+// Multer setup for uploading images
+const storage = multer.diskStorage({
+    destination: function(req, file, cb)
+    {
+      cb(null, './public/images/upload');
+    },
+    filename: function(req, file, cb)
+    {
+      crypto.randomBytes(12,function(err,bytes)
+      {
+        const fn=bytes.toString('hex')+path.extname(file.originalname);
+        cb(null, fn);
+      });
+    },
+  });
+  const upload = multer({ storage: storage });
 
+
+// router.route("/addUser").post(userController.addUser);
+router.route("/addUser").post(upload.single("image"), userController.addUser);
 
 // router.route("/getAllUsers")
 //             .get(
