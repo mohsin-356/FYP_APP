4fbcfb220b8c1bfbdb6931590e9314f7896dd314
mohsin-356 <mohsinsaeed356@gmail.com>
2025-02-09 18:44:31 +0500

    Refactor user model to streamline schema; replace fullname with userName, add role and address fields, and include image URL support

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -3,33 +3,21 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 //here we defien our user schema for uber driver
 const userSchema = new mongoose.Schema({
-  fullname: {
-    firstname: {
-      type: String,
-      required: true,
-      minlength: [3, "Too short, min is 3 characters"],
-    },
-    lastname: {
-      type: String,
-      required: true,
-      minlength: [3, "Too short, min is 3 characters"],
-    },
+  userName: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  phone: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true, enum: ['Admin', 'Manager', 'Supplier', 'Worker'] },
+  cnic: { type: String, required: true, unique: true },
+  address: {
+      street: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+      province: { type: String, required: true },
+      postalCode: { type: String, required: true }
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    minlength: [5, "Too short, min is 6 characters"]
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  socketId: {
-    type: String,
-  }
-});
+  image: { type: String, default: null }, // Image URL
+}, { timestamps: true });
 //creating some methods for the user schema
 userSchema.methods.generateAuthToken =function () {
     const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
